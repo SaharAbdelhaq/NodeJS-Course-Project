de3d9c0ad4a99d12dc84bd4df67f7d13b8123327
Sahar Abdelhaq <saharabdelhaq02@gmail.com>
2024-05-09 08:17:53 +0300

    validation added

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,3 +1,5 @@
+const { check, validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [{ title: "First Post", content: "This is the First Post" }],
@@ -5,6 +7,14 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      Message: "Validation failed, enterd data incorrect",
+      errors: errors.array(),
+    });
+  }
+
   const title = req.body.title;
   const content = req.body.content;
 
